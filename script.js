27a8d0aeb6adc74edad690d40172aeee7522991e
ainedoherty <ainecdoherty97@icloud.com>
2024-05-18 13:27:29 +0100

    full website coded

---
+++ script.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var portfolioButton = document.getElementById("portfolioButton");
+    var portfolioButton2 = document.getElementById("portfolioButton2");
+
+    // Add click event listener
+    portfolioButton.addEventListener("click", function() {
+        // Redirect to portfolio.html
+        window.location.href = "portfolio.html";
+    });
+
+     // Add click event listener
+     portfolioButton2.addEventListener("click", function() {
+        // Redirect to portfolio.html
+        window.location.href = "portfolio.html";
+    });
+});
